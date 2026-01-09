0d704e93a43fb69d9682139497f11302fe787f24
Mohammed Abbas <mohammedz17890@gmail.com>
2026-01-09 15:54:53 +0300

    Complete exercise

---
+++ Brute Force Attack-06.py
@@ -0,0 +1,16 @@
+
+password = 12345
+attempts = 5
+
+while attempts > 0:
+    user_input = int(input("Enter password: "))
+
+    if user_input == password:
+        print("Access granted")
+        break
+    else:
+        attempts -= 1
+        print("Wrong password. Attempts left:", attempts)
+
+if attempts == 0:
+    print("Authorities have been alerted")
